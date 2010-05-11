beb1bf4d298545a83dd8bbbeab6d5ee0b31e1e70
Johan Eckerström <johan@duh.se>
2010-05-11 16:43:18 +0200

    Rewrite the realtime plot example to a while loop

--- plottermos.m
+++ plottermos.m
@@ -1,6 +1,5 @@
 % the data
-     nt=5; % <- traces
-     np=1000; % <- data/trace
+     np=10000; % <- data/trace
 % prepare the plot
      axes('xlim',[1,np],'ylim',[-2,3]);
      x=1:np;
@@ -12,22 +11,32 @@
             'linestyle','none');
      lb=line([inf,inf],[-2,3]);
      shg;
-% prepare a simple timer function
-     com=[
-          'ix=get(lh,''userdata'')+1;',...
-          'ix=rem(ix-1,np)+1;'...
-          'y(ix)=rand;'...
-          'set(lb,''xdata'',[ix,ix]);'...
-          'set(lh,''ydata'',y,''userdata'',ix);'
-     ];
-% create the timer
-     th=timer;
-     set(th,...
-        'timerfcn',com,...
-        'period',.05,...
-        'executionmode','fixedrate');
-% ...and start it
-     start(th);
-% use
-% stop(th);
-% delete(th);
+% % prepare a simple timer function
+%      com=[
+%           'ix=get(lh,''userdata'')+1;',...
+%           'ix=rem(ix-1,np)+1;'...
+%           'y(ix)=rand;'...
+%           'set(lb,''xdata'',[ix,ix]);'...
+%           'set(lh,''ydata'',y,''userdata'',ix);'
+%      ];
+% % create the timer
+%      th=timer;
+%      set(th,...
+%         'timerfcn',com,...
+%         'period',.05,...
+%         'executionmode','fixedrate');
+% % ...and start it
+%      start(th);
+% % use
+% % stop(th);
+% % delete(th);
+ix = 1;
+
+while(true)
+    pause(0.05);
+    ix = get(lh, 'userdata') + 1;
+    ix = rem(ix - 1, np) + 1;
+    y(ix) = rand;
+    set(lb, 'xdata', [ix, ix]);
+    set(lh, 'ydata', y, 'userdata', ix);
+end % while
